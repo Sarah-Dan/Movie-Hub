30435e33071d1054085cad82493a495386428264
Sarah Daniel <sarahdanbolt@gmail.com>
2022-09-13 10:31:20 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
+// greetings component
+function Greetings(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>Hello there! Good morning!</p>
+  )
+}
+// avatar component
+
+
+// userProfileCard component
+function UserProfileCard(){
+  return (
+    <div className="card">
+      <img className="card-image" src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{width:100}}/>
+      <div className="card-name">Sarah Daniel</div>
+      <div>Sarah Daniel is a Software Engineer</div>
     </div>
+  )
+}
+
+// list component
+function ListComponent(){
+  return <li>Tesla</li>
+}
+
+// root component
+function App(){
+  return (
+    <section className="main-page">
+      <UserProfileCard/>
+    </section>
   );
 }
 
