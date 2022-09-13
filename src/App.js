d5dc5b55b0eba5812d9106ae0ab644a501b0f9e7
Sarah Daniel <sarahdanbolt@gmail.com>
2022-09-13 14:55:47 +0100

    Added a new card component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React from "react"
+// import Avatar from "./images/myapp-sarah.jpeg"
 
 // greetings component
 function Greetings(){
   return (
-    <p>Hello there! Good morning!</p>
+    <p>Hello there! Welcome to Tech Newbie!</p>
   )
 }
-// avatar component
 
+// avatar component
+function Avatar(){
+  return(
+<img className="card-image" src={require("./images/myapp-sarah.jpeg")} alt="Avatar" style={{width:100}}/>
+);
+}
+function Avatar2(){
+  return(
+<img className="card-image" src={require("./images/myapp-austin.jpeg")} alt="Avatar" style={{width:100}}/>
+);
+}
 
 // userProfileCard component
 function UserProfileCard(){
   return (
     <div className="card">
-      <img className="card-image" src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{width:100}}/>
+      <Avatar/>
       <div className="card-name">Sarah Daniel</div>
-      <div>Sarah Daniel is a Software Engineer</div>
+      <div className="bio">
+      <div>Sarah Daniel is a Software Engineer, CX Manger, Dancer, and Business woman.</div>
+      <div>Lives in Lagos, Nigeria</div>
+      <div>Loves learning new things, cooking, movies, good music, reading, and traveling.</div>
+      <div>Connect on Twitter <a href="https://twitter.com/saahdan">Sarah</a></div>
+      </div>
+    </div>
+  )
+}
+
+function UserProfileCard2(){
+  return (
+    <div className="card">
+      <Avatar2/>
+      <div className="card-name">Austin Ayodele</div>
+      <div className="bio">
+      <div>Austin Ayodele is a Software Engineer, Web developer, and Tech Community leader.</div>
+      <div>Lives in Lagos, Nigeria</div>
+      <div>Loves coding, collaborating with other developers, and watching anime.</div>
+      <div>Connect on Twitter <a href="https://twitter.com/ausstinab?s=11&t=V-8DDrWx1kj580-JKl63ag">Austin</a></div>
+      </div>
     </div>
   )
 }
@@ -29,7 +60,9 @@ function ListComponent(){
 function App(){
   return (
     <section className="main-page">
+      <Greetings/>
       <UserProfileCard/>
+      <UserProfileCard2/>
     </section>
   );
 }
